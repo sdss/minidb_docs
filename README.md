b68f19762d7dbd3ba9d907237c9733e588321db2
José Sánchez-Gallego <gallegoj@uw.edu>
2022-09-12 10:32:38 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# minidb_docs
+
+This repository contains table descriptions for the contents of minidb for DR18. To add descriptions to the tables, add text under the `Summary` section or after the each columns (after the ` - `). You can use the GitHub editor if you want.
---
+++ create_docs_files.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2022-09-12
+# @Filename: create_docs_files.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+from __future__ import annotations
+
+from sdssdb.peewee.sdss5db import database
+
+
+def create_docs_files():
+
+    database.set_profile('tunnel_operations')
+
+    tables = database.get_tables('minidb')
+    for table in tables:
+        f = open(str(table) + '.txt', 'w')
+
+        f.write('Summary\n')
+        f.write('-------\n')
+        f.write('\n\n\n\n')
+
+        f.write('Columns\n')
+        f.write('-------\n')
+        f.write('\n')
+
+        columns = database.get_columns(str(table), 'minidb')
+        for column in columns:
+            f.write(str(column.name) + ' - \n')
+
+        f.write('\n')
+        f.close()
+
+
+if __name__ == "__main__":
+    create_docs_files()
