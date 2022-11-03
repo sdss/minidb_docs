b2ec83cddf50107092c86f57a8d252748f112d6c
José Sánchez-Gallego <gallegoj@uw.edu>
2022-11-03 14:39:12 -0700

    Update create_docs_files.py script

--- create_docs_files.py
+++ create_docs_files.py
@@ -7,6 +7,7 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
 from __future__ import annotations
+
 import os
 import warnings
 
@@ -15,30 +16,30 @@ from sdssdb.peewee.sdss5db import database
 
 def create_docs_files():
 
-    database.set_profile('tunnel_operations')
+    database.set_profile("tunnel_operations")
 
-    tables = database.get_tables('minidb')
+    tables = database.get_tables("minidb")
     for table in tables:
-        fname = str(table) + '.txt'
+        fname = str(table) + ".txt"
         if os.path.exists(fname):
-            warnings.warn(f'{fname} already exists')
+            warnings.warn(f"{fname} already exists")
             continue
 
-        f = open(fname, 'w')
+        f = open(fname, "w")
 
-        f.write('Summary\n')
-        f.write('-------\n')
-        f.write('\n\n\n\n')
+        f.write("Summary\n")
+        f.write("-------\n")
+        f.write("\n\n\n\n")
 
-        f.write('Columns\n')
-        f.write('-------\n')
-        f.write('\n')
+        f.write("Columns\n")
+        f.write("-------\n")
+        f.write("\n")
 
-        columns = database.get_columns(str(table), 'minidb')
+        columns = database.get_columns(str(table), "minidb")
         for column in columns:
-            f.write(str(column.name) + ' - \n')
+            f.write(str(column.name) + " - \n")
 
-        f.write('\n')
+        f.write("\n")
         f.close()
 
 
